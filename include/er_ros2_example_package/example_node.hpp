d2a256d6b0600f64ee8f02003fb39d1b1cdf6497
ExplainingRobotics <explainingrobotics@outlook.com>
2022-08-07 17:20:02 +0200

    Add Function descriptions

--- include/er_ros2_example_package/example_node.hpp
+++ include/er_ros2_example_package/example_node.hpp
@@ -68,35 +68,85 @@ protected:
 
 
 private:
-    void publishTimer();
-    void registerPublisher();
-    void registerSubscriber();
-    void registerServices();
-    void registerActions();
-    void registerParameters();
-    void callbackString(std_msgs::msg::String::ConstSharedPtr msg);
-    void onParamEvent(rcl_interfaces::msg::ParameterEvent::UniquePtr event);
-    void callbackService(const std::shared_ptr<rmw_request_id_t> request_header, const std::shared_ptr<std_srvs::srv::SetBool::Request> request, const std::shared_ptr<std_srvs::srv::SetBool::Response> response);
-    rclcpp_action::CancelResponse handleCancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::Fibonacci>> goal_handle);
-    void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::Fibonacci>> goal_handle);
-    rclcpp_action::GoalResponse handleGoal(const rclcpp_action::GoalUUID & uuid,std::shared_ptr<const example_interfaces::action::Fibonacci::Goal> goal);
-    void handleAccepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::Fibonacci>> goal_handle);
-    
-    bool activate_lifecycle_;
-    
-    long count_;
-    bool positive_ = true;
-    std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
-    rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr param_sub;
-    rclcpp::AsyncParametersClient::SharedPtr param_client_;
-    std::string example_parameter_;
-    rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::String>::SharedPtr pub_string_;
-    rclcpp::TimerBase::SharedPtr timer_publish_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_string_;
+  /**
+   * @brief Timer Callback to publish Data
+   */
+  void publishTimer();
+  /**
+   * @brief Registers all publishers
+   */
+  void registerPublisher();
+  /**
+   * @brief Registers all subscribers
+   */
+  void registerSubscriber();
+  /**
+   * @brief Registers all services
+   */
+  void registerServices();
+  /**
+   * @brief Registers all actions
+   */
+  void registerActions();
+  /**
+   * @brief Handles Parameter Server and changes
+   */
+  void registerParameters();
+  /**
+   * @brief Callback for the Subscriber
+   * @param msg incomming message
+   */
+  void callbackString(std_msgs::msg::String::ConstSharedPtr msg);
+  /**
+   * @brief Callback to handle the Parameter Change event
+   * @param event incomming message
+   */
+  void onParamEvent(rcl_interfaces::msg::ParameterEvent::UniquePtr event);
+  /**
+   * @brief Callback to handle the SetBool Service
+   * @param request_header ros2 header
+   * @param request input request message
+   * @param response output response message
+   */
+  void callbackService(const std::shared_ptr<rmw_request_id_t> request_header, const std::shared_ptr<std_srvs::srv::SetBool::Request> request, const std::shared_ptr<std_srvs::srv::SetBool::Response> response);
+  /**
+   * @brief Callback to handle Cancling of the Action
+   * @param goal_handle The goal handle to cancle
+   * @return ACCEPT or REJECT
+   */
+  rclcpp_action::CancelResponse handleCancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::Fibonacci>> goal_handle);
+  /**
+   * @brief Callback to for executing the Goal
+   * @param goal_handle The goal handle to cancle
+   */
+  void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::Fibonacci>> goal_handle);
+  /**
+   * @brief Callback to handle activating the Goal of the Action
+   * @param goal The goal
+   * @return ACCEPT or REJECT
+   */
+  rclcpp_action::GoalResponse handleGoal(const rclcpp_action::GoalUUID & uuid,std::shared_ptr<const example_interfaces::action::Fibonacci::Goal> goal);
+  /**
+   * @brief Callback after accepting before executing the goal
+   * @param goal_handle The goal handle to cancle
+   */
+  void handleAccepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::Fibonacci>> goal_handle);
+  
+  bool activate_lifecycle_;
+  
+  long count_;
+  bool positive_ = true;
+  std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
+  rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr param_sub;
+  rclcpp::AsyncParametersClient::SharedPtr param_client_;
+  std::string example_parameter_;
+  rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::String>::SharedPtr pub_string_;
+  rclcpp::TimerBase::SharedPtr timer_publish_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_string_;
 
-    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr service_set_bool_;
-    rclcpp_action::Server<example_interfaces::action::Fibonacci>::SharedPtr action_server_;
-    Eigen::Vector2d eigen_vector_;
+  rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr service_set_bool_;
+  rclcpp_action::Server<example_interfaces::action::Fibonacci>::SharedPtr action_server_;
+  Eigen::Vector2d eigen_vector_;
 };
 
 } // namespace
